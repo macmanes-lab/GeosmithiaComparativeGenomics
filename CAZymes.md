36c69fb5c95df3e61a9c4529ff30aaaddf6df161
Taruna <ta2007@wildcats.unh.edu>
2016-02-03 20:22:03 -0500

    Adding functional version of macse4cdsOrthofiles_TA.py

---
+++ CAZymes.md
@@ -0,0 +1,3 @@
+```
+hmmscan --domtblout gm5.cazymes.out.dm --cpu 16 ../../bin/cazymesNpfam/dbCAN-fam-HMMs.txt ../../gm5/functional/gm5_proteins_final_unwrapped.fasta > gm5_cazymes_dbcan_log.out
+```
---
+++ scripts4PAML/autoPAML.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+usage=$(cat << EOF
+   # This script runs a pipeline that takes a fasta file and BAMfiles and tests for selection:
+   #
+   autopaml.sh [options]
+   Options:
+      -t <v> : *required* Numberof threads to use.
+EOF
+);
+
+
+while getopts f:b:o:t: option
+do
+        case "${option}"
+        in
+		t) TC=${OPTARG};;
+        esac
+done
+
+
+
+##Align
+END=$(ls | wc -l | awk '{print $1}')
+START=1
+
+
+for inputaln in $(ls *aln); do
+    F=`basename $inputaln .aln`;
+    if [ $(ps -U ta2007 | grep 'codeml\|raxmlHPC-PTHREADS' | wc -l | awk '{print $1}') -lt $TC ] ;
+    then
+        echo 'I have a core to use'
+        #java -Xmx2000m -jar ~/bin/MACSE/macse_v1.01b.jar -prog alignSequences -seq $inputaln -out_NT $F.aln &&
+        raxmlHPC-PTHREADS-AVX -T 2 -m GTRGAMMA -n $F.tree -s $F.aln -p 12345 &&
+        python ~/myscripts/testing_PAML/cds4paml/autoPAML_nullbranchsite.py $F.aln RAxML_bestTree.$F.tree $F.out &
+    else
+        echo 'Dont wake me up until there is something else to do'
+        sleep 25s
+    fi
+done
