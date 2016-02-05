8086df85063dc8a8aafa00667cd9bf0517cf62f0
Taruna <ta2007@wildcats.unh.edu>
2016-02-05 10:36:39 -0500

    Converting optional args into required args in trimal script

--- Scripts4phylogeny/trimal4Orthofiles_TA.py
+++ Scripts4phylogeny/trimal4Orthofiles_TA.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-# A program for trimal'ing all files that contain protein sequences of single-copy orthologs.
+# A program for trimal'ing all files that contain protein sequences of single-copy orthologs 
+# generated with Orthofinder version 0.3.0
 # USAGE: ./trimal4Orthofiles_TA.py
 # Author: Taruna Aggarwal
 # Affiliation: University of New Hampshire, Durham, NH, USA
 # Date: 01/27/2016
-# Purpose is trim protein sequences using trimAl v1.4.rev15 with -gt option set to 0.20
+# Purpose is trim protein sequences using trimAl v1.4.rev15 with -gappyout option
+
 # Usage: ./trimal4Orthofiles_TA.py --root dir_with_all_orthofinder_files --trimal_dir path_to_output_dir
 
 import os
@@ -12,14 +14,14 @@ import subprocess
 import argparse
 
 
-parser = argparse.ArgumentParser(description="This script runs trimal for files in a directory.")
-parser.add_argument('--root', help="PATH to the directory containing orthogroup files.")
-parser.add_argument('--trimal_dir', help="PATH to the directory for trimmed orthogroup files.")
+parser = argparse.ArgumentParser(description="This script runs trimal on a set of sequence files in a directory.")
+parser.add_argument('--root', help="PATH to the directory containing orthogroup files.", required=True)
+parser.add_argument('--trimal_dir', help="PATH to the directory for trimmed orthogroup files.", required=True)
 args = parser.parse_args()
 
 
 def runtrimal(input_file, output_file):
-    trimal_command = "trimal -in {0} -out {1} -gt 0.20".format(input_file, output_file)
+    trimal_command = "trimal -in {0} -out {1} -gappyout".format(input_file, output_file)
     subprocess.call(trimal_command, shell=True)
 
 
