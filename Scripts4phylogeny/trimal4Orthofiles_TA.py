360e130c5bf2cdfd2e91b74158479fb6bb08322e
Taruna <ta2007@wildcats.unh.edu>
2016-02-05 10:09:23 -0500

    Updating trimal script

--- Scripts4phylogeny/trimal4Orthofiles_TA.py
+++ Scripts4phylogeny/trimal4Orthofiles_TA.py
@@ -4,24 +4,23 @@
 # Author: Taruna Aggarwal
 # Affiliation: University of New Hampshire, Durham, NH, USA
 # Date: 01/27/2016
-# Purpose is
+# Purpose is trim protein sequences using trimAl v1.4.rev15 with -gt option set to 0.20
+# Usage: ./trimal4Orthofiles_TA.py --root dir_with_all_orthofinder_files --trimal_dir path_to_output_dir
 
-import sys
 import os
 import subprocess
 import argparse
-import shutil
+
 
 parser = argparse.ArgumentParser(description="This script runs trimal for files in a directory.")
-parser.add_argument('--root', default="/home/mcclintock/ta2007/myscripts/orthogroupScripts/parsed_orthogroups_len16/", help="PATH to the directory containing orthogroup files.")
-parser.add_argument('--trimal_dir', default="/home/mcclintock/ta2007/myscripts/orthogroupScripts/trimal_orthogroups_len16_gt20percent/", help="PATH to the directory for trimmed orthogroup files.")
+parser.add_argument('--root', help="PATH to the directory containing orthogroup files.")
+parser.add_argument('--trimal_dir', help="PATH to the directory for trimmed orthogroup files.")
 args = parser.parse_args()
 
 
 def runtrimal(input_file, output_file):
-    trimal_command = "/home/mcclintock/ta2007/bin/trimal/source/trimal -in {0} -out {1} -gt 0.20".format(input_file, output_file)
+    trimal_command = "trimal -in {0} -out {1} -gt 0.20".format(input_file, output_file)
     subprocess.call(trimal_command, shell=True)
-
 
 
 Orig_file_dir = args.root
