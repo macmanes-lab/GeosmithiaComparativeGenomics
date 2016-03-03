f4702315c67c4bdf4ad24db4ec633e0f9fa82c0f
Taruna <ta2007@wildcats.unh.edu>
2016-03-03 10:08:04 -0500

    updating paml scripts

--- Scripts4PAML/3_autoPAML_short.sh
+++ Scripts4PAML/3_autoPAML_short.sh
@@ -23,15 +23,16 @@ START=1
 
 for inputaln in $(ls *fasta); do
     F=`basename $inputaln .fasta`;
-    if [ $(ps -all | grep 'java\|codeml\|raxmlHPC' | wc -l | awk '{print $1}') -lt $TC ] ;
+    if [ $(ps -U aggarwal | grep 'java\|codeml\|raxmlHPC' | wc -l | awk '{print $1}') -lt $TC ] ;
     then
         echo 'I have a core to use'
-        java -Xmx2000m -jar /fungi/taruna/shared/bin/MACESE/macse_v1.01b.jar -prog alignSequences -seq $inputaln -out_NT $F.aln &&
+        java -Xmx2000m -jar /fungi/taruna/shared/bin/MACSE/macse_v1.01b.jar -prog alignSequences -seq $inputaln -out_NT $F.aln &&
         perl /fungi/taruna/shared/bin/pal2nal.v14/pal2nal.pl $F'_macse_AA.fasta' $F.aln -output fasta -nogap -nomismatch > $F.clean &&
         raxmlHPC-PTHREADS -f a -m GTRGAMMA -p 83845 -T 1 -x 93458 -N 100 -n $F.tree -s $F.clean &&
-        INSERT remove_hashtag.py && 
+        ./removehashtag_TA.py $F.clean $F.fixed.clean && 
         python null_autoPAML.py $F.fixed.clean RAxML_bestTree.$F.tree $F.null.out &&
-        python alt_autoPAML.py $F.fixed.clean RAxML_bestTree.$F.tree $F.alt.out &
+        python alt_autoPAML.py $F.fixed.clean RAxML_bestTree.$F.tree $F.alt.out &&
+        python results_paml.py $F.null.out $F.alt.out | tee -a geosmithia_PAML.results &
     else
         echo 'Dont wake me up until there is something else to do'
         sleep 25s
